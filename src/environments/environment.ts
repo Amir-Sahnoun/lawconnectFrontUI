629ff499f391bc7dcfc6b1d94c68a1a5aff8ab33
Amir-Sahnoun <amir.sahnoun@esprit.tn>
2025-05-13 16:38:52 +0100

    another url change -_-

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backend_url: 'https://20e5-74-220-27-36.ngrok-free.app',
-  apiUrl: 'https://20e5-74-220-27-36.ngrok-free.app',
+  backend_url: 'https://ea57-74-220-27-36.ngrok-free.app',
+  apiUrl: 'https://ea57-74-220-27-36.ngrok-free.app',
   prefix: 'api'
 };
